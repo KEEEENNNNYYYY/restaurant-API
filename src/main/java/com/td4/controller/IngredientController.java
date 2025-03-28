654446b21177166da6cff0de6aa6c7fc7073ef32
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-03-28 08:23:43 +0100

    chore : initialization for ingredientController

--- src/main/java/com/td4/controller/IngredientController.java
+++ src/main/java/com/td4/controller/IngredientController.java
@@ -1,4 +1,28 @@
 package com.td4.controller;
 
+
+import com.td4.DAO.IngredientDAO;
+import com.td4.model.Ingredient;
+import com.td4.service.IngredientService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/Ingredient")
 public class IngredientController {
+    private final IngredientService ingredientService;
+    private IngredientDAO ingredientDAO;
+
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    @GetMapping("/all")
+    public List<Ingredient> getAllIngredient() {
+        return ingredientService.getAllIngredient();
+    }
+
 }
