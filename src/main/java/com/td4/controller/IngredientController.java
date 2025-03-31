7a85af6224944aa750d4a896f1b16605bf74aef0
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-03-31 10:38:34 +0200

    refactor : macthing type as database for id

--- src/main/java/com/td4/controller/IngredientController.java
+++ src/main/java/com/td4/controller/IngredientController.java
@@ -3,13 +3,14 @@ package com.td4.controller;
 
 import com.td4.DAO.IngredientDAO;
 import com.td4.DTO.IngredientDTO;
-import com.td4.model.Ingredient;
 import com.td4.service.IngredientService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/Ingredient")
@@ -24,6 +25,11 @@ public class IngredientController {
     @GetMapping("/all")
     public List<IngredientDTO> getAllIngredient() {
         return ingredientService.getAllIngredient();
+    }
+
+    @GetMapping("/{id}")
+    public IngredientDTO getIngredientById(@PathVariable Integer id) {
+        return ingredientService.getIngredientById(id);
     }
 
 }
