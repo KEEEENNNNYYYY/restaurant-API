7d2126e2495b68e1a0f64f30212d4e3a26c6309a
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-03-31 10:37:30 +0200

    refactor : macthing type as database for id

--- src/main/java/com/td4/DTO/IngredientDTO.java
+++ src/main/java/com/td4/DTO/IngredientDTO.java
@@ -3,14 +3,15 @@ package com.td4.DTO;
 import com.td4.model.Unit;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class IngredientDTO {
-    private String id;
+    private Integer id;
     private String name;
     private Double UnitPrice;
     private LocalDate updatedAt;
 
-    public IngredientDTO(String id, String name, Double unitPrice, LocalDate updatedAt) {
+    public IngredientDTO(Integer id, String name, Double unitPrice, LocalDate updatedAt) {
         this.id = id;
         this.name = name;
         UnitPrice = unitPrice;
@@ -21,11 +22,11 @@ public class IngredientDTO {
 
     }
 
-    public String getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -51,5 +52,28 @@ public class IngredientDTO {
 
     public void setUpdatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IngredientDTO that = (IngredientDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(UnitPrice, that.UnitPrice) && Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, UnitPrice, updatedAt);
+    }
+
+    @Override
+    public String toString() {
+        return "IngredientDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", UnitPrice=" + UnitPrice +
+                ", updatedAt=" + updatedAt +
+                '}';
     }
 }
